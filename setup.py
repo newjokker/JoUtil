64ab22e0a85be07923b6411f5119be0c2bd0b7d8
jokker <18761609908@163.com>
2020-12-04 20:13:50 +0800

    * 修改不能打包的 bug

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ from setuptools import setup, find_packages
 setup(
 
     name='JoUtil',                                                                          # 打包起来的包的文件名
-    version='0.1.9',                                                                        # 版本
+    version='0.1.10',                                                                        # 版本
     description='a tools for TXKJ algo',                                                    # 描述
     author='jokker',                                                                        # 作者
     author_email='18761609908@163.com',                                                     # 邮箱
+
     requires=['numpy', 'pillow', 'pandas', 'easydict', 'matplotlib', 'imagehash', 'prettytable', 'progress',
-              'progressbar', 'requests', 'imageio', 'pyexiv2', 'cv2', 'scikit-image', 'exifread', 'whatimage', 'pyheif',
-              'pymysql', 'pytesseract', 'beautifulsoup4', 'Crypto', 'scikit-learn'],          # 定义依赖哪些模块
+              'progressbar', 'requests', 'imageio', 'pyexiv2', 'cv2', 'exifread', 'whatimage', 'pyheif',
+              'pymysql', 'pytesseract', 'beautifulsoup4', 'Crypto',
+              ],          # 定义依赖哪些模块
+
     # 打包的python文件夹
     packages=['JoTools', 'JoTools/utils', 'JoTools/txkj', 'JoTools/for_csdn',
               'JoTools/for_csdn/word_pic', 'JoTools/for_csdn/find_same_img', 'JoTools/for_csdn/the_art_of_war'
@@ -32,5 +35,6 @@ setup(
 # 注意
 # 需要将用到的包全部写到 packages 参数后
 # 需要在 setup.py 同级目录创建一个 test.py 文件用于测试
+# requires 不能出现 *-* 格式的写法 如 scikit-image，否则会报错
 
 # setuptools 的进一步学习参考：https://www.jianshu.com/p/ea9973091fdf

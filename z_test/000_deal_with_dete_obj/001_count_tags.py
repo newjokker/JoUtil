0c44d6a5ada3ef31450421ea54bab748a842b972
jokker <18761609908@163.com>
2021-09-16 19:06:56 +0800

    * 增加上下文管理器

--- z_test/000_deal_with_dete_obj/001_count_tags.py
+++ z_test/000_deal_with_dete_obj/001_count_tags.py
@@ -23,5 +23,6 @@ if __name__ == "__main__":
         OperateDeteRes.get_class_count(args.xml_dir, print_count=True)
     else:
         xml_dir = r"C:\Users\14271\Desktop\save_res"
+        # xml_dir = r"C:\Users\14271\Desktop\save_res_del"
         OperateDeteRes.get_class_count(xml_dir, print_count=True)
 

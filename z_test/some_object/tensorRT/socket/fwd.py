2c8a7ac409f83d98cc0765500541b8be3f83eab2
jokker <18761609908@163.com>
2022-02-11 15:38:50 +0800

    * 增加锁帧操作

--- z_test/some_object/tensorRT/socket/fwd.py
+++ z_test/some_object/tensorRT/socket/fwd.py
@@ -1,23 +1,73 @@
 # -*- coding: utf-8  -*-
 # -*- author: jokker -*-
 
-###服务器端server.py
 import socket
-import os
-import sys
 import struct
 import uuid
-import cv2
 import numpy as np
+import time
+import argparse
+import cv2
+import json
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+monkey.patch_all()
+from flask import Flask, request, jsonify
+import threading
+import configparser
+import ctypes
+import gc
+import os, sys
+import subprocess
+import subprocess as sp
+
+this_dir = os.path.dirname(__file__)
+lib_path = os.path.join(this_dir, '..')
+sys.path.insert(0, lib_path)
+
+from lib.detect_libs.yolov5RT import Yolov5RT
+from JoTools.utils.FileOperationUtil import FileOperationUtil
+
+# --------------------------------------
+sys.path.insert(0, r"/home/tensorRT/tensorrtx/yolov5")
+PLUGIN_LIBRARY = "/home/tensorRT/tensorrtx/yolov5/build/libmyplugins.so"
+ctypes.CDLL(PLUGIN_LIBRARY)
+
+
+
+class FrameCal():
+
+    def __init__(self, length):
+        self.length = length
+        self.time_list = []
+
+    def tag(self):
+        if len(self.time_list) == self.length:
+            self.time_list.pop(0)
+        self.time_list.append(time.time())
+
+    def get_frame(self):
+        if len(self.time_list) <= 1:
+            return -1
+        else:
+            start = self.time_list[0]
+            end = self.time_list[-1]
+            if start == end:
+                return -1
+            else:
+                return (len(self.time_list) -1)/(time.time() - self.time_list[0])
+
+    def get_length(self):
+        return len(self.time_list)
 
 
 def socket_service_image():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # s.bind(('127.0.0.1', 6666))
-        s.bind(('0.0.0.0', 1122))
-        s.listen(10)
+        s.bind(('0.0.0.0', 1211))
+        # fixme 这边是设置的监听的时间
+        s.listen(10000)
     except socket.error as msg:
         print(msg)
         sys.exit(1)
@@ -30,15 +80,14 @@ def socket_service_image():
 
 
 def deal_image(sock, addr):
-    print("Accept connection from {0}".format(addr))  # 查看发送端的ip和端口
+    print("Accept connection from {0}".format(addr))                    # 查看发送端的ip和端口
 
     while True:
         fileinfo_size = struct.calcsize('128sq')
         buf = sock.recv(fileinfo_size)
         if buf:
             filename, filesize = struct.unpack('128sq', buf)
-            fn = filename.decode().strip('\x00')
-            new_filename = os.path.join('./res/', 'new_{0}'.format(str(uuid.uuid1())) + fn)
+            fn = filename.decode().strip('\x00')                        # file name
 
             recvd_size = 0
             res = b""
@@ -53,18 +102,96 @@ def deal_image(sock, addr):
                     recvd_size = filesize
                     res += data
 
-            print(len(res))
+
+            # fixme 增加帧率锁定的功能
+
+
+            print('-'*30 + 'frame')
+            now_frame = fc.get_frame()
+            print(now_frame)
+            print('-'*30 + 'frame')
+
+            if now_frame > 20:
+                print('*skip*')
+                continue
+
             img_np_arr = np.fromstring(res, np.uint8)
-            im = cv2.imdecode(img_np_arr, cv2.COLOR_BGR2RGB)
+            frame = cv2.imdecode(img_np_arr, cv2.COLOR_BGR2RGB)
+
+            # ----------------------------------------------------------------------------------------------------------
+            dete_res = model.detectSOUT(image=frame)
+            dete_res.print_as_fzc_format()
+            dete_res.draw_dete_res(r"./res/res.jpg", assign_img=frame)
+            #
+            p.stdin.write(frame.tostring())
+
+            fc.tag()
+            # ----------------------------------------------------------------------------------------------------------
 
         sock.close()
         break
 
 
-if __name__ == '__main__':
-    index = 0
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tensorflow Faster R-CNN demo')
+    parser.add_argument('--gpuID', dest='gpuID', type=int, default=0)
+    parser.add_argument('--port', dest='port', type=int, default=5444)
+    parser.add_argument('--gpuRatio', dest='gpuRatio', type=float, default=0.1)
+    parser.add_argument('--host', dest='host', type=str, default='127.0.0.1')
+    parser.add_argument('--logID', dest='logID', type=str, default='0')
+    parser.add_argument('--objName', dest='objName', type=str, default='')
+    args = parser.parse_args()
+    return args
 
-    a = ['1', '2', '3']
+
+
+if __name__ == '__main__':
+
+
+    stack = []
+
+    fc = FrameCal(100)
+
+    start_time = time.time()
+    args = parse_args()
+    # portNum = args.port
+    # host = args.host
+    portNum = 1211
+    host = "0.0.0.0"
+
+    objName = "yolov5_rt_aqm"
+    scriptName = "aqm"
+    # --------------------------------------------------------
+    model = Yolov5RT(args, objName, scriptName)
+    model.model_restore()
+    # --------------------------------------------------------
+    # rtmpUrl = "rtsp://192.168.3.221/live"
+    rtmpUrl = "rtmp://192.168.3.99/123/221"
+    w, h = 1280, 720
+    # w, h = 676, 342
+    # w, h = 556, 368
+    video_fps = 15
+    # -----
+    command = ['ffmpeg',
+               '-y',
+               '-f', 'rawvideo',
+               '-vcodec', 'rawvideo',
+               '-pix_fmt', 'bgr24',
+               '-thread_queue_size', '512',
+               '-s', "{}x{}".format(w, h),
+               '-r', str(video_fps),
+               '-i', '-',
+               '-c:v', 'libx264',
+               '-pix_fmt', 'yuv420p',
+               '-preset', 'ultrafast',
+               '-tune', 'zerolatency',
+               '-sc_threshold', '499',
+               '-rtsp_transport', 'tcp',
+               '-f', 'rtsp',
+               rtmpUrl]
+
+    # 管道配置
+    p = sp.Popen(command, stdin=sp.PIPE)
 
     socket_service_image()
 
--- z_test/some_object/tensorRT/socket/khd_rtsp.py
+++ z_test/some_object/tensorRT/socket/khd_rtsp.py
@@ -12,6 +12,8 @@ import os
 import sys
 import struct
 import time
+import cv2
+
 
 def sock_client_image():
 
@@ -21,33 +23,39 @@ def sock_client_image():
 
     index = 0
 
+    cap = cv2.VideoCapture(r"rtsp://admin:txkj-2021@192.168.3.17:554/Streaming/Channels/501")
+
     while True:
 
-        if time.time() - start_time > 200:
+        if time.time() - start_time > 200000:
             break
 
+        ret, frame = cap.read()
+        # ret, frame = cap.read()
+        # ret, frame = cap.read()
+
+        if ret:
+            #
+            success, encoded_image = cv2.imencode(".jpg", frame)
+            byte_data = encoded_image.tobytes()
+        else:
+            continue
+
         index += 1
-        print(index)
+        # print(index)
+
+        print(index/(time.time()-start_time))
 
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect(('192.168.3.221', 1211))  # 服务器和客户端在不同的系统或不同的主机下时使用的ip和端口，首先要查看服务器所在的系统网卡的ip
+            fhead = struct.pack(b'128sq', bytes(os.path.basename(filepath), encoding='utf-8'), len(byte_data))  # 将xxx.jpg以128sq的格式打包
+            s.send(fhead)
+            s.send(byte_data)  # 以二进制格式发送图片数据
+            # s.close()
         except socket.error as msg:
             print(msg)
             print(sys.exit(1))
-
-        fhead = struct.pack(b'128sq', bytes(os.path.basename(filepath), encoding='utf-8'), os.stat(filepath).st_size)  # 将xxx.jpg以128sq的格式打包
-        s.send(fhead)
-
-
-        fp = open(filepath, 'rb')  # 打开要传输的图片
-        while True:
-            data = fp.read()  # 读入图片数据
-            if not data:
-                print('{0} send over...'.format(filepath))
-                break
-            s.send(data)  # 以二进制格式发送图片数据
-        s.close()
 
 
 if __name__ == '__main__':
